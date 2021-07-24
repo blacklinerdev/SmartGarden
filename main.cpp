da9c9ad9b95043c5a698120a4bdcf649c67ffe67
Leroy <Leroy>
2021-07-24 19:51:32 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sstream>
+#define PORT 2040
+
+using namespace std;
+
+vector<string> split(char[]);
+void init(int&, struct sockaddr_in&);
+
+int main()
+{
+    int socket_fd, client;
+    int portnum = 2040;
+    int buffersize = 1024;
+    char buffer[buffersize];
+    bool running = true;
+    struct sockaddr_in server_address;
+    socklen_t size;
+
+    // init socket
+    init(socket_fd, server_address);
+
+    //listening
+    while(running)
+    {
+        cout << "Listening..." << endl;
+        listen(socket_fd, 1);
+
+        //accept client
+        client = accept(socket_fd, (struct sockaddr*)&server_address, &size);
+        if(client < 0)
+        {
+            cout << "Error accepting clients" << endl;
+            exit(3);
+        }
+        //send/recieve
+        recv(client, buffer, buffersize, 0);
+        cout << buffer << endl;
+        send(client, buffer, buffersize, 0);
+    }
+}
+void init(int& socket_fd, struct sockaddr_in& server_address)
+{
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(socket_fd < 0)
+    {
+        cout << "\nSocket creation failed" << endl;
+        exit(-1);
+    }
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(PORT);
+    int binder = bind(socket_fd, (struct sockaddr*)&server_address, sizeof(server_address));
+    if(binder < 0)
+    {
+        cout << "\nSocket binding failed" << endl;
+        exit(-2);
+    }
+}
+vector<string> split(char buffer[])
+{
+    vector<string> result;
+    int i = 0;
+    for(int j = 0; j < sizeof(buffer); j++)
+    {
+        if(buffer[j] == ' ' || j == sizeof(buffer))
+        {
+            string word;
+            for(i; i < j - 1; i++)
+            {
+                word += buffer[i];
+            }
+            result.push_back(word);
+            i = j + 1;
+        }
+    }
+}
